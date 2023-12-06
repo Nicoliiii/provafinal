03004c3ce384c250796e38ef06b9f20db372e014
Nicoliiii <119983473+Nicoliiii@users.noreply.github.com>
2023-12-06 00:44:00 +0000

    fix:teste02-erro

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,47 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import books from '../components/books'
+import { useState } from 'react'
+
 export default function Home() {
-  return (
-    <h1>teste</h1>
+  const [books, atualizarBook] = useState<any[]>([])
+
+  function adicionarBook(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    let title = e.target[0].value
+    let page = e.target[1].value
+   
+    if (title.length == 0 || page.length == 0) {
+      alert('Preencha todos os campos')
+      return 
+    }
+    const novo_book = {
+      title: title,
+      page: page,
+    } 
+    atualizarBook([...books, novo_book])
+  }
+  function removerBook(title: string) {
+    let novos_books = books.filter(books => books.title != title) 
+
+    atualizarBook(novos_books)
+  }
+  return(
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1>Gerenciador de Cadastro de Livros</h1>
+      <div>
+      <form onSubmit={adicionarBook} className='w-full flex flex-col space-y-4 my-10'>
+      <input type="text" title="title" placeholder='Nome do Livro'/> 
+          <input type="text" name="page" placeholder='Quantidade de Paginas'/> 
+          <button type="submit" className='border-2 rounded-lg border-green-300 bg-green-200'>Adicionar</button>
+      </form>
+      </div>
+      <div id="lista" className='space-y-5 w-full max-w-2xl'>
+        {books.map(Book01 => <books
+          key={Book01.title}
+          nome={Book01.page} 
+          removerBook={removerBook} 
+        />)}
+      </div>
+    </main>
   )
+  
 }
---
+++ components/books.tsx
@@ -0,0 +1,18 @@
+type Props = {
+    title: string
+    page: string
+    removerBook: Function 
+ }
+ export default function books(props: Props) {
+    return (
+        <div className="flex w-full space-x-10 items-center border-2 border-gray-200 bg-gray-50 rounded-lg p-2">
+                <img src={props.title} className="aspect-square w-auto h-20 object-cover"/>
+            <div className="w-full">
+                <p>
+                    {props.page}
+                </p>
+            </div>
+            <button className="border-2 border-red-300 bg-red-50 px-2 rounded-lg h-min" onClick={() => props.removerBook(props.title)}>Remover</button>
+        </div>
+    )
+  }
